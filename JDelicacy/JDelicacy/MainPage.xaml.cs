3cc5f47702a355ea4c8fe45570c0f96b17d7e8da
Nathan situ <nathansitu@hotmail.com>
2017-07-24 21:05:43 +1200

    Added Mainpage UI

---
+++ JDelicacy/JDelicacy/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JDelicacy
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void ShareButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void UploadButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void TakeButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+	}
+}
